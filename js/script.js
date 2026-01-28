e4fee76a29006e351c3cd5379c62236e24a5a930
chiaracariolato <chiaracariolato@gmail.com>
2026-01-28 20:08:04 +0100

    fix: fixed ; syntax

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ if(eta < 18) {
     let prezzo = ((km * 0.21) * 0.80);
     console.log("Il prezzo del biglietto è " + prezzo.toFixed(2) + "€");
 } else if(eta >= 65) {
-    let prezzo = ((km * 0.21) * 0.60)
+    let prezzo = ((km * 0.21) * 0.60);
     console.log("Il prezzo del biglietto è " + prezzo.toFixed(2) + "€");
 } else {
     let prezzo = (km * 0.21);
